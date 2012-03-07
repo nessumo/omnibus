1ec845431f447825bd7c3590ccf0b04d5864ec63
nessumo <nessumo@sanrensei.net>
2012-03-07 22:07:13 +0100

    basic web mechanisms configured

--- omnibus/settings.py
+++ omnibus/settings.py
@@ -38,18 +38,18 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = ROOT_PATH + 'media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = ROOT_PATH + 'static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -60,6 +60,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    ROOT_PATH + 'design/',
 )
 
 # List of finder classes that know how to find static files in
@@ -99,6 +100,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    ROOT_PATH + 'templates/',
 )
 
 INSTALLED_APPS = (
--- omnibus/urls.py
+++ omnibus/urls.py
@@ -1,13 +1,17 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.views.generic.simple import direct_to_template
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
 
-    # url(r'^$', 'omnibus.views.home', name='home'),
+    url(r'^$', direct_to_template, {'template': 'page.html'}, name='home'),
     # url(r'^omnibus/', include('omnibus.foo.urls')),
 
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += staticfiles_urlpatterns()
